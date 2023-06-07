c3d0dfd285c2768dfff512837b364619e3dd340a
osmangony01 <mdosmangoni376@gmail.com>
2023-06-07 21:24:07 +0600

    add users route to save user in database form client-side

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
+const jwt = require('jsonwebtoken');
 
 
 const app = express();
@@ -11,46 +12,70 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res)=>{
+app.get("/", (req, res) => {
     res.send("Summar Camp is running...")
 })
+
+
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.l6kpz6n.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
 
-    const userCollection = client.db('summarCamp').collection('users');
-
-
-    app.post("/users", (req, res)=>{
-        const user = req.body;
-        console.log(user);
-        
-    })
+        const userCollection = client.db('summarCamp').collection('users');
 
 
+        app.post("/users", async (req, res) => {
+            const user = req.body;
+            //console.log(user);
+            const query = {email: user.email};
+            const existingUser = await userCollection.findOne(query);
+            //console.log(existingUser);
+            if(existingUser){
+                return res.send({message: 'user already exists'});
+            }
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        })
+
+        app.get("/users", async(req, res)=>{
+            const result = await userCollection.find().toArray();
+            res.send(result);
+        })
+
+        // jwt token
+        app.post("/jwt", (req, res) => {
+            const user = req.body;
+            console.log(user);
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: '1h' });
+            res.send({ token });
+        })
 
 
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    //await client.close();
-  }
+
+
+
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        //await client.close();
+    }
 }
 run().catch(console.dir);
 
@@ -58,6 +83,6 @@ run().catch(console.dir);
 
 
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`Summar camp is running on PORT : ${PORT}`)
 })
