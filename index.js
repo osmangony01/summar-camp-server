42562532cf5a176c43bddc8e149368d2fc512688
osmangony01 <mdosmangoni376@gmail.com>
2023-06-12 02:35:52 +0600

    add delete route to delete selected class from client-side

--- index.js
+++ index.js
@@ -288,6 +288,14 @@ async function run() {
             res.send(result);
         })
 
+        // delete selected class
+        app.delete("/delete-selected-class/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await selectedClassCollection.deleteOne(query);
+            console.log(id);
+            res.send(result);
+        })
 
 
         await client.db("admin").command({ ping: 1 });
